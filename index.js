93a498481c19398bceedc54fd08d001b683b532b
Shafiul <shafiulahcb1007@gmail.com>
2022-04-25 23:46:54 +0600

    version 1.0

---
+++ index.js
@@ -0,0 +1,37 @@
+function addMore(){
+    document.getElementById("error").innerHTML = "";
+
+    let text = document.getElementById("input-text").value;
+
+    if(text==""){
+        document.getElementById("error").innerHTML = "Please add some task";
+    }
+    else{
+        let input_text = document.getElementById("lists");
+        let li = document.createElement('li');
+        li.textContent = text;
+
+        let a = document.createElement('a');
+        a.textContent = "X";
+        a.href = "javascript:void(0)";
+        a.className="remove";
+        li.appendChild(a);
+
+        let pos = input_text.firstElementChild;
+
+        if(pos==null){
+            input_text.appendChild(li);    
+        }
+        else{
+            input_text.insertBefore(li,pos);
+        }
+    }
+    document.getElementById("input-text").value = "";
+}
+
+let btn = document.querySelector('ul');
+btn.addEventListener('click',function(e){
+    let input_text = document.getElementById('lists');
+    let li = e.target.parentNode;
+    input_text.removeChild(li);
+});
